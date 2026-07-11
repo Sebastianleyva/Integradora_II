4429b85b39c8c7239cc96cfa5e4bd66650ad3751
Emiliano-Cardiel <emiliano_3141240252@utd.edu.mx>
2026-07-11 14:59:20 -0600

    Arreglar deploy de render

--- backend/model/info.js
+++ backend/model/info.js
@@ -11,8 +11,8 @@ app.set("trust proxy", 1);
 
 //Aplicación y puerto
 const app = express();
+app.set("trust proxy", 1);
 const port = process.env.PORT || 5000;
-
 //Restricciones
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
