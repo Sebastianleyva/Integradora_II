9d17ca72985c2e0d010f9ccc83a2b2e06444510c
Emiliano-Cardiel <emiliano_3141240252@utd.edu.mx>
2026-07-11 11:23:58 -0600

    Configuración de backend para Supabase

--- backend/model/info.js
+++ backend/model/info.js
@@ -8,7 +8,7 @@ import pool from "../database/index.js";
 
 //Aplicación y puerto
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //Restricciones
 app.use(bodyParser.json({ limit: "50mb" }));
@@ -38,12 +38,10 @@ app.post("/account/register", async (req, res) => {
     const ver = "SELECT * FROM login_cred($1);";
     const exist = await pool.query(ver, [correo]);
     if (exist.rows.length > 0) {
-      return res
-        .status(409)
-        .json({
-          message:
-            "Este correo ya esta registrado. Intenta con otro correo o inicia sesion si es tu cuenta.",
-        });
+      return res.status(409).json({
+        message:
+          "Este correo ya esta registrado. Intenta con otro correo o inicia sesion si es tu cuenta.",
+      });
     }
     const fecha = new Date().toISOString().split("T")[0];
     const contraHash = await bcrypt.hash(contra, 10);
@@ -76,13 +74,11 @@ app.post("/account/register", async (req, res) => {
       correo: usuario.rows[0].correo,
     };
 
-    return res
-      .status(201)
-      .json({
-        message:
-          "Tu cuenta ha sido creada correctamente. Ahora puedes iniciar sesión.",
-        result: usuario.rows || usuario,
-      });
+    return res.status(201).json({
+      message:
+        "Tu cuenta ha sido creada correctamente. Ahora puedes iniciar sesión.",
+      result: usuario.rows || usuario,
+    });
   } catch (err) {
     console.error(err);
     return res.status(500).json({ error: err.message || err });
@@ -99,12 +95,10 @@ app.post("/account/login", async (req, res) => {
     console.log(correo, contra);
     if (result.rows.length === 0) {
       console.error("Usuario no encontrado");
-      return res
-        .status(404)
-        .json({
-          message:
-            "No existe una cuenta con este correo. ¿Quizas querías registrarte?",
-        });
+      return res.status(404).json({
+        message:
+          "No existe una cuenta con este correo. ¿Quizas querías registrarte?",
+      });
     }
 
     const usuario = result.rows[0];
@@ -112,12 +106,10 @@ app.post("/account/login", async (req, res) => {
 
     if (!ver) {
       console.error("La contraseña es incorrecta");
-      return res
-        .status(401)
-        .json({
-          message:
-            "La contraseña es incorrecta, Verifica tus datos e intenta de nuevo.",
-        });
+      return res.status(401).json({
+        message:
+          "La contraseña es incorrecta, Verifica tus datos e intenta de nuevo.",
+      });
     }
 
     // Guardar datos en la sesión
@@ -150,7 +142,7 @@ app.get("/account/logout", (req, res) => {
       console.error(err);
       return res
         .status(500)
-        .json({error: `No se pudo cerrar la sesión: ${err.message || err}`});
+        .json({ error: `No se pudo cerrar la sesión: ${err.message || err}` });
     } else {
       console.log("Sesión cerrada");
       return res.status(200).send("Sesión cerrada");
@@ -164,27 +156,35 @@ app.put("/account/update-password/:id", async (req, res) => {
     const { id } = req.params;
     const { contra, newcontra } = req.body;
     if (contra.trim() && newcontra.trim()) {
-      const sql = "SELECT * FROM alumno($1);"
+      const sql = "SELECT * FROM alumno($1);";
       const result = await pool.query(sql, [id]);
       if (result.rows.length == 0) {
         console.error("Usuario no encontrado");
-        return res.status(404).json({message: "No hay una sección activa disponible"});
+        return res
+          .status(404)
+          .json({ message: "No hay una sección activa disponible" });
       } else {
         const alumno = result.rows[0];
         const ver = await bcrypt.compare(contra, alumno.contrasena);
         if (!ver) {
           console.error("Contraseña incorrecta");
-          return res.status(401).json({message: "La contraseña es incorrecta"});
+          return res
+            .status(401)
+            .json({ message: "La contraseña es incorrecta" });
         } else {
           const contraHash = await bcrypt.hash(newcontra, 10);
           const sql2 = "CALL pass_change($1, $2)";
           const updateRes = await pool.query(sql2, [id, contraHash]);
           console.info({ updateRes });
-          return res.status(200).json({message: "Contraseña cambiada con éxito"});
+          return res
+            .status(200)
+            .json({ message: "Contraseña cambiada con éxito" });
         }
       }
     } else {
-      return res.status(400).json({message: "Introduce las contraseñas correspondientes"});
+      return res
+        .status(400)
+        .json({ message: "Introduce las contraseñas correspondientes" });
     }
   } catch (err) {
     console.error(err);
@@ -246,7 +246,7 @@ app.post("/general/:id", async (req, res) => {
       actividad,
       psiquia,
       psico,
-      work
+      work,
     } = req.body;
     if (
       !Number.isFinite(edad) ||
@@ -276,7 +276,8 @@ app.post("/general/:id", async (req, res) => {
       });
     } else {
       // const result = await pool.query(sql, [ide, edad, sexo, carrera, instituto, fecha, n_insc, burnout, actividad, psiquia, psico, id]);
-      const sql = "SELECT * FROM general_insert($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);";
+      const sql =
+        "SELECT * FROM general_insert($1, $2, $3, $4, $5, $6, $7, $8, $9, $10);";
       const result = await pool.query(sql, [
         edad,
         sexo,
@@ -287,7 +288,7 @@ app.post("/general/:id", async (req, res) => {
         psiquia,
         psico,
         work,
-        id
+        id,
       ]);
       return res.status(201).send({ result });
     }
@@ -308,13 +309,11 @@ app.get("/general/:id", async (req, res) => {
         .status(200)
         .json({ encuesta: false, message: "Inicio de sesión exitoso" });
     } else {
-      return res
-        .status(200)
-        .json({
-          encuesta: true,
-          message:
-            "Inicio de sesión logrado, por favor realiza la encuesta general",
-        });
+      return res.status(200).json({
+        encuesta: true,
+        message:
+          "Inicio de sesión logrado, por favor realiza la encuesta general",
+      });
     }
   } catch (err) {
     console.error(err.message || err);
@@ -338,7 +337,7 @@ app.post("/registros/:id", async (req, res) => {
       pregunta_objetivo,
     } = req.body;
     const fecha = new Date().toISOString().split("T")[0];
-    const existe = await pool.query("SELECT * FROM verified($1)", [id],);
+    const existe = await pool.query("SELECT * FROM verified($1)", [id]);
     if (existe.rows.length > 0) {
       return res.status(409).json({
         error: "Ya existe un registro para hoy, muchas gracias por responder",
@@ -355,22 +354,33 @@ app.post("/registros/:id", async (req, res) => {
         !Number.isFinite(Number(pregunta_objetivo))
       ) {
         console.log(
-          "id: ", id,
-          "\nhoras de sueno: ", h_sueno,
-          "\nCalidad de consumo: ", cal_sueno,
-          "\nNúmeros de comidas: ", n_comidas,
-          "\nCalidad de consumo: ", cal_consumo,
-          "\nHoras de osio: ", h_osio,
-          "\nCalidad de consumo tec: ", cal_consumo_tec,
-          "\nUso de la IA: ", uso_ia,
-          "\nAplicación: ", aplicacion,
-          "\nPregunta objetivo: ", pregunta_objetivo,
+          "id: ",
+          id,
+          "\nhoras de sueno: ",
+          h_sueno,
+          "\nCalidad de consumo: ",
+          cal_sueno,
+          "\nNúmeros de comidas: ",
+          n_comidas,
+          "\nCalidad de consumo: ",
+          cal_consumo,
+          "\nHoras de osio: ",
+          h_osio,
+          "\nCalidad de consumo tec: ",
+          cal_consumo_tec,
+          "\nUso de la IA: ",
+          uso_ia,
+          "\nAplicación: ",
+          aplicacion,
+          "\nPregunta objetivo: ",
+          pregunta_objetivo,
         );
         return res
           .status(400)
           .send({ error: "No se han insertado los datos correspondientes" });
       } else {
-        const sql = "CALL register_insert($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);";
+        const sql =
+          "CALL register_insert($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11);";
         const result = await pool.query(sql, [
           fecha,
           Number(h_sueno),
